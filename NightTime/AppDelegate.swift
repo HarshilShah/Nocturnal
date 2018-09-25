34c252b9f3565d055a0402e40b356f2e6564e27f
Harshil Shah <harshilshah1910@outlook.com>
2018-09-25 18:47:57 +0530

    Initial Commit

---
+++ NightTime/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NightTime
+//
+//  Created by Harshil Shah on 25/09/18.
+//  Copyright © 2018 Harshil Shah. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NightTime/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NightTime
+//
+//  Created by Harshil Shah on 25/09/18.
+//  Copyright © 2018 Harshil Shah. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
