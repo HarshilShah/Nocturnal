0d1e9007a50748a3ca317fcb589204d1d01077b1
Harshil Shah <harshilshah1910@outlook.com>
2018-09-26 17:56:59 +0530

    Initial implementation

--- NightTime/AppDelegate.swift
+++ NightTime/AppDelegate.swift
@@ -9,17 +9,54 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
+    private lazy var statusItem: NSStatusItem = {
+        return NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+    }()
+    
+    private lazy var menu: NSMenu = {
+        let menu = NSMenu()
+        menu.addItem(withTitle: "Quit NightTime",
+                     action: #selector(NSApplication.terminate),
+                     keyEquivalent: "q")
+        return menu
+    }()
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.button?.target = self
+        statusItem.button?.action = #selector(itemWasClicked(_:))
+        statusItem.button?.sendAction(on: [.leftMouseDown, .rightMouseUp])
+        statusItem.button?.image = NSImage(named: NSImage.Name("MenuBarIcon"))
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    @objc private func itemWasClicked(_: NSStatusBarButton) {
+        guard let event = NSApp.currentEvent else {
+            return
+        }
+        
+        if event.type == .rightMouseUp || event.modifierFlags.contains(.option) {
+            toggleTheme()
+        } else {
+            showMenu()
+        }
     }
-
+    
+    private func showMenu() {
+        statusItem.menu = menu
+        statusItem.popUpMenu(menu)
+        statusItem.menu = nil
+    }
+    
+    private func toggleTheme() {
+        let script = """
+        tell application "System Events"
+            tell appearance preferences
+                set dark mode to not dark mode
+            end tell
+        end tell
+        """
+        
+        NSAppleScript(source: script)?.executeAndReturnError(nil)
+    }
 
 }
-
