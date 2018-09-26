fb4e6abc6b596b2228483be768565ccceffddb29
Harshil Shah <harshilshah1910@outlook.com>
2018-09-26 12:10:40 +0530

    Removed storyboards

---
+++ NightTime/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  NightTime
+//
+//  Created by Harshil Shah on 26/09/18.
+//  Copyright © 2018 Harshil Shah. All rights reserved.
+//
+
+import Cocoa
+
+let delegate = AppDelegate()
+NSApplication.shared.delegate = delegate
+
+let ret = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
