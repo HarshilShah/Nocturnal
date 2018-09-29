5f70be3c15753f55b78b095db89ee3dd5926332f
Harshil Shah <harshilshah1910@outlook.com>
2018-09-29 11:25:29 +0530

    Removed the menu to quit the app

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -14,37 +14,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
     }()
     
-    private lazy var menu: NSMenu = {
-        let menu = NSMenu()
-        menu.addItem(withTitle: "Quit",
-                     action: #selector(NSApplication.terminate),
-                     keyEquivalent: "q")
-        return menu
-    }()
-    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        statusItem.behavior = .terminationOnRemoval
         statusItem.button?.target = self
         statusItem.button?.action = #selector(itemWasClicked(_:))
-        statusItem.button?.sendAction(on: [.leftMouseDown, .rightMouseUp])
+        statusItem.button?.sendAction(on: [.leftMouseUp, .rightMouseUp])
         statusItem.button?.image = NSImage(named: NSImage.Name("MenuBarIcon"))
     }
     
     @objc private func itemWasClicked(_: NSStatusBarButton) {
-        guard let event = NSApp.currentEvent else {
-            return
-        }
-        
-        if event.type == .rightMouseUp || event.modifierFlags.contains(.option) {
-            toggleTheme()
-        } else {
-            showMenu()
-        }
-    }
-    
-    private func showMenu() {
-        statusItem.menu = menu
-        statusItem.popUpMenu(menu)
-        statusItem.menu = nil
+        toggleTheme()
     }
     
     private func toggleTheme() {
@@ -58,5 +37,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         NSAppleScript(source: script)?.executeAndReturnError(nil)
     }
-
+    
 }
